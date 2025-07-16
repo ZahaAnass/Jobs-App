951da763054853fd171efaf673973d3b17500fc3
ZahaAnass <zahaanass277@gmail.com>
2025-07-16 17:25:23 +0100

    change update method from put to patch

--- routes/jobs.js
+++ routes/jobs.js
@@ -5,6 +5,6 @@ const router = express.Router()
 const { getAllJobs, getJob, createJob, updateJob, deleteJob } = require("../controllers/jobs")
 
 router.route("/").get(getAllJobs).post(createJob)
-router.route("/:id").get(getJob).put(updateJob).delete(deleteJob)
+router.route("/:id").get(getJob).patch(updateJob).delete(deleteJob)
 
 module.exports = router
